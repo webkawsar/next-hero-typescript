5879198706589b1195ead7aef5f8656da416a187
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-01-18 18:52:09 +0600

    data types practice

---
+++ module-1/src/1.4 Basic Data Types.ts
@@ -0,0 +1,40 @@
+
+// Data Types
+// Primitive Data Types
+// Non Primitive Data Types
+
+// primitive
+// string
+// number
+// boolean
+// null
+// undefined
+// symbol
+
+
+// non primitive
+// array
+// object
+// tuple
+
+
+// implicit defined data
+// explicit defined data
+
+const fullName:string = 'Kawsar Ahmed';
+const age:number = 28;
+const isAdmin:boolean = true;
+const x:null = null;
+const y:undefined = undefined;
+
+// any type
+let z;
+
+// array type
+const friends:string[] = ['Samim', 'Imran'];
+
+// tuple
+const info:[string, number] = ['Kawsar', 28];
+
+
+
