737fc5920f975a869bb657ce83ba49170c1f1d62
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-10-21 23:45:39 +0600

    practice object, optional type, literal type

--- module-1/src/1.5-reference-types.ts
+++ module-1/src/1.5-reference-types.ts
@@ -1,9 +1,9 @@
-
+// Reference Type --> Object
 const user:{
     firstName: string; 
     lastName: string; 
     age?:number // optional type
-    readonly company: 'Dream Engineer' // literal type
+    readonly company: 'Dream Engineer' // type --> literal type
 } = {firstName: 'Kawsar', lastName: 'Ahmed', company: 'Dream Engineer'}
 
 
