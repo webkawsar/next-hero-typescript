1781e9ea66bfc2f95054d8e5276cd478875935b2
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-11-14 23:58:53 +0600

    practice type, interface, type vs interface

--- module-2/src/2.2-type-interface.ts
+++ module-2/src/2.2-type-interface.ts
@@ -25,11 +25,35 @@
         role: string
     }
 
+    // type ke interface er sathe use kora jay
+    // interface UserWithRole2 extends User1 {
+    //     role: string
+    // }
+
     const user:UserWithRole2 = {
         name: 'Kawsar Ahmed',
         age: 29,
         role: 'Admin'
     }
+
+    // JS ---> Object, array --> object, function --> object
+    type Roll1 = number[];
+    interface Roll2 {
+        [index:number]: number
+    }
+
+    // const userNumbers:Roll1 = [1, 3, 5];
+    const userNumbers:Roll2 = [1, 3, 5];
+
+
+    // function er sathe type, interface use korar system
+    type Add1 = (num1:number, num2:number) => number;
+    interface Add2 {
+        (num1:number, num2:number): number
+    }
+
+    // const add:Add1 = (num1, num2) => num1 + num2;
+    const add:Add2 = (num1, num2) => num1 + num2;
 
 
     //
