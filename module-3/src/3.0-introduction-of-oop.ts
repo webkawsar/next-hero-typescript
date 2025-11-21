cf3c2a71e1399170cee706d267c05d7ac36463a8
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-11-21 23:38:14 +0600

    practice class, property, method, access modifier, parameter properties

---
+++ module-3/src/3.0-introduction-of-oop.ts
@@ -0,0 +1,22 @@
+{
+    // Introduction of Object Oriented Programming
+    // what is oop
+    // programming paradigm
+    // procedural programming
+    // functional programming
+    // oop programming
+    // declarative programming
+
+
+    // Building blocks of OOP
+    // inheritance
+    // polymorphism
+    // abstraction
+    // encapsulation
+
+
+
+    
+
+    //
+}
---
+++ module-3/src/3.1-class-and-object.ts
@@ -0,0 +1,31 @@
+{
+    // OOP --> Class
+    class Animal {
+        // public name: string;
+        // public specifications: string;
+        // public sound: string;
+
+        // parameter properties
+        constructor(public name: string, public specifications: string, public sound: string) {
+            // this.name = name;
+            // this.specifications = specifications;
+            // this.sound = sound;
+        }
+
+        makeSound() {
+            console.log(`The ${dog.name} make sound like ${this.sound}`);
+        }
+    }
+
+    const dog = new Animal("Kutta", "dog", "Ghew Ghew");
+    // dog.name
+    // dog.makeSound()
+
+
+
+
+
+
+
+    //
+}
