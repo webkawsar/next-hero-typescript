8f435f5ac0065e80e270281256c65176a1454fe9
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-11-29 17:46:52 +0600

    practice how static property, method works

--- module-3/src/3.7-statics-in-oop.ts
+++ module-3/src/3.7-statics-in-oop.ts
@@ -1,27 +1,44 @@
 {
     // Statics in OOP
     class Counter {
-        count: number = 0;
+        // count: number = 0;
+        // increment() {
+        //     return this.count = this.count + 1;
+        // }
 
-        increment() {
-            this.count = this.count + 1;
+        // decrement() {
+        //     return this.count = this.count - 1;
+        // }
+
+
+        static count: number = 0;
+        static increment() {
+            return Counter.count = Counter.count + 1;
         }
 
-        decrement() {
-            this.count = this.count - 1;
+        static decrement() {
+            return Counter.count = Counter.count - 1;
         }
     }
 
-    const count1 = new Counter();
-    console.log(count1.increment());
-    console.log(count1.increment());
-    console.log(count1.increment());
-    console.log(count1.increment());
+    // const counter1 = new Counter();
+    // console.log(counter1.increment());
+    // console.log(counter1.increment());
+    // console.log(counter1.increment());
+    // console.log(counter1.increment());
+    console.log(Counter.increment())
+    console.log(Counter.increment())
+    console.log(Counter.increment())
 
 
-    const count2 = new Counter();
-    console.log(count2.decrement());
-    console.log(count2.decrement());
+    // const counter2 = new Counter();
+    // console.log(counter2.decrement());
+    // console.log(counter2.decrement());
+    console.log(Counter.decrement());
+    console.log(Counter.decrement());
+
+    // static sara korle different memory create hoy
+    // static add korle eki memory ke reference kore
 
 
 
