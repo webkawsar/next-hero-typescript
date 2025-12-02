fc04a57c45b7b9c9dcca16cd6527cc949c79c950
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-12-03 00:26:39 +0600

    solved problem-3

--- module-3/src/assignment-1.ts
+++ module-3/src/assignment-1.ts
@@ -51,9 +51,27 @@ console.log(findLargestNumber(sampleArray));
 
 
 
+// Problem 3:
+// Create a TypeScript function filterEvenNumbers that takes an array of numbers and returns a new array containing only the even numbers from the original array.
+// Sample Input:
+// filterEvenNumbers([1, 2, 3, 4, 5, 6])
 
+// Sample Output:
+// [2, 4, 6]
 
+const filterEvenNumbers = (numberArray:number[]):number[] => {
+    let result:number[] = [];
+    for (let i = 0; i < numberArray.length; i++) {
+        const element = numberArray[i]!;
+        if(element%2 === 0){
+            result.push(element);
+        }
+    }
 
+    return result;
+}
+
+console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]));
 
 
 
