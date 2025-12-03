b6c292faddf45b6e5c74bc952069492239a951a2
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-12-03 22:00:09 +0600

    solved problem-7

--- module-3/src/assignment-1.ts
+++ module-3/src/assignment-1.ts
@@ -194,12 +194,31 @@ console.log(isRecentBook(book1));
 
 
 
+// Problem 7:
+// Create a TypeScript function logString that takes a parameter of type unknown and uses a type guard to check whether the parameter is of type string. If it is, log the string to the console. If it is not, log an error message.
 
+// Sample Input 1: 
+// logString("Hello, TypeScript!");
 
+// Sample Output 1:  
+// "Hello, TypeScript!"
 
+// Sample Input 2: 
+// logString(42)
 
+// Output: Error: 
+// Input is not a string.
 
+const logString = (param: unknown):void => {
+    if(typeof param === 'string') {
+        console.log(param);
+    } else {
+        console.log("Error: Input is not a string.");
+    }
+}
 
+logString("Hello, TypeScript!");
+logString(42);
 
 
 
