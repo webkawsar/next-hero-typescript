e808a10a5efa9f30aa87e84df308aad8452d7377
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-12-03 08:10:54 +0600

    solved problem-4

--- module-3/src/assignment-1.ts
+++ module-3/src/assignment-1.ts
@@ -59,11 +59,12 @@ console.log(findLargestNumber(sampleArray));
 // Sample Output:
 // [2, 4, 6]
 
+// way-1
 const filterEvenNumbers = (numberArray:number[]):number[] => {
     let result:number[] = [];
     for (let i = 0; i < numberArray.length; i++) {
-        const element = numberArray[i]!;
-        if(element%2 === 0){
+        const element = numberArray[i]!; // ekhane eta (!) deya mane ami confirm er value undefined hobe na
+        if((element%2) === 0){
             result.push(element);
         }
     }
@@ -72,6 +73,42 @@ const filterEvenNumbers = (numberArray:number[]):number[] => {
 }
 
 console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]));
+
+
+
+
+// Problem 4:
+// Design a TypeScript function reverseArray that takes an array of any type and returns a new array with its elements reversed. Use generics to define types.
+
+// Sample Input 1:
+// reverseArray(["apple", "banana", "cherry"])
+
+// Sample Output 1:
+// ["cherry", "banana", "apple"]
+
+// Sample Input 2:
+// reverseArray([10, 20, 30])
+
+// Sample Output 2: 
+// [30, 20, 10]
+
+
+const reverseArray = <T>(param: T[]) => {
+    let result: T[] = [];
+    for (const element of param) {
+        result.unshift(element);
+    }
+
+    return result;
+}
+
+const result = reverseArray<string>(["apple", "banana", "cherry"]);
+const result2 = reverseArray<number>([10, 20, 30]);
+console.log(result) ;
+console.log(result2) ;
+
+
+
 
 
 
