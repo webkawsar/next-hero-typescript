ec82bac0de090f14110fc09d0e80d15aa0399cef
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-12-07 08:18:44 +0600

    practice mongodb operator

---
+++ module-5/5.3-mongodb-opearator.ts
@@ -0,0 +1,23 @@
+// Mongo DB Operator
+
+// $eq
+// db_name.collection_name.find({gender: {$eq: "female"}});
+// db_name.collection_name.find({age: {$eq: 29}});
+
+// $ne
+// db_name.collection_name.find({age: {$ne: 29}});
+
+// $gt --> greater than
+// db_name.collection_name.find({age: {$gt: 29}});
+
+// $gte --> greater than or equal
+// db_name.collection_name.find({age: {$gt: 29}}).sort({age: 1});
+
+// $lte --> less than or equal
+// db_name.collection_name.find({age: {$lte: 29}}).sort({age: 1});
+
+
+
+
+
+
